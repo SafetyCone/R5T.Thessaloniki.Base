6a889b4d973ce30a023f45349c8d94e74a8c6c86
MinexAutomation <minex.automation@gmail.com>
2019-12-29 01:53:43 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Thessaloniki.Base.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Thessaloniki.Base.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
